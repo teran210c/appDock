330e17b4863ccb7daa0e46e61649e8f6f761f76c
Javier <a242729@uach.mx>
2024-06-22 15:01:05 -0600

    primer commit

---
+++ src/javaapplication12/JavaApplication12.java
@@ -0,0 +1,11 @@
+
+package javaapplication12;
+
+public class JavaApplication12 {
+
+    public static void main(String[] args) {
+        System.out.println("Mi aplicacion en Java!");
+        
+    }
+    
+}
